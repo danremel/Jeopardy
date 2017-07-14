adde72b0134ecdcde78bb72bdd2f911d7447dc6b
danremel <remel.dan@gmail.com>
2017-07-14 10:38:46 -0400

    Added comments to every line, breaking down and explaining what each command is doing in this current version.

--- javascript/main.js
+++ javascript/main.js
@@ -39,16 +39,26 @@ for (i = 0; i < QuestionSet.length; i++){
 
 
 // ## CREATE BUTTON FUNCTIONALITY ##
-$('.divs').click(function () {
-	var playerInput = prompt(this.getAttribute("question"));
-	var makeLowerCase = playerInput.toLowerCase();
-	var points = this.getAttribute("points");
-	if (makeLowerCase === this.getAttribute("answer")) {
-		alert("You are correct!")
-	} else {
-		alert("You are incorrect.")
-	}
-})
+
 // GIVEN user selects a box
 // WHEN box is clicked
+$('.divs').click(function () {
 // THEN the box reveals the question in a pop-up box
+	var playerInput = prompt(this.getAttribute("question"))
+	// This command makes any uppercase letters in the response
+	// turn into lowercase letters, creating uniformity and matching
+	// the answer criteria.
+	var makeLowerCase = playerInput.toLowerCase();
+	// This variable links var points with the "points" property
+	// in the QuestionSet object array.
+	var points = this.getAttribute("points");
+	// IF user's answer matches the string in the "answer" property,
+	// THEN return alert saying "You are correct."
+	// ELSE return alert saying "You are incorrect.""
+	if (makeLowerCase === this.getAttribute("answer")) {
+		alert("You are correct!");
+	} else {
+		alert("You are incorrect.");
+	}
+})
+
